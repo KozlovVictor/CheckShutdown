a369abebe1aec77a603c7ef651f7baefaef0a277
Admin <hostelv@list.ru>
2018-04-27 15:06:37 +0600

    add server address in args

--- src/ru/myprojects/client/Client.java
+++ src/ru/myprojects/client/Client.java
@@ -10,25 +10,26 @@ public class Client {
 
     private final int PORT = 8189;
     private String serverAddress;
-    private Socket socket = null;
-    private DataInputStream in = null;
+    private Socket socket;
+    /*private DataInputStream in = null;
     private DataOutputStream out = null;
-    private String serverMessage = null;
-    //private Inet4Address ip = null;
+    private String serverMessage = null;*/
 
     public Client() {
         try {
             serverAddress = "localhost";
             socket = new Socket(serverAddress, PORT);
             System.out.println("Connected with " + serverAddress);
-            in = new DataInputStream(socket.getInputStream());
+            while(true){
+            }
+            /*in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
             while (true) {
                 serverMessage = in.readUTF();
                 if (serverMessage.equals("info")) {
                     getInfo();
                 }
-            }
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -39,25 +40,25 @@ public class Client {
         try {
             socket = new Socket(serverAddress, PORT);
             System.out.println("Connected with " + serverAddress);
-            in = new DataInputStream(socket.getInputStream());
+            /*in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
             while (true) {
                 serverMessage = in.readUTF();
                 if (serverMessage.equals("info")) {
                     getInfo();
                 }
-            }
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void getInfo() {
+    /*private void getInfo() {
         try {
             out.writeUTF(InetAddress.getLocalHost().getCanonicalHostName());
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 }
--- src/ru/myprojects/server/Server.java
+++ src/ru/myprojects/server/Server.java
@@ -1,57 +1,75 @@
 package ru.myprojects.server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Server {
     private final int PORT = 8189;
     private final int TIME_DELAY = 5000;
-    private ServerSocket serverSocket = null;
-    private Socket socket = null;
-    private DataInputStream in = null;
-    private DataOutputStream out = null;
-    private Timer delay = null;
-    //private Arr
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+    /*private DataInputStream in = null;
+    private DataOutputStream out = null;*/
+    private Timer delay;
+    private ArrayList<Socket> sockets;
 
 
     public Server() {
+        sockets = new ArrayList<>();
         try {
             serverSocket = new ServerSocket(PORT);
             System.out.println("Server started! Wait a clients.");
-            socket = serverSocket.accept();
-            System.out.println("Client connected succesfully.");
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            new Thread(() -> {
-                while (true) {
-                    String clientMessage = null;
-                    try {
-                        clientMessage = in.readUTF();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println(clientMessage);
-                }
-            }).start();
             delay = new Timer();
-            delay.schedule(new TimerTask() {
+            TimerTask sorting = new TimerTask() {
                 @Override
                 public void run() {
-                    try {
-                        out.writeUTF("info");
-                        out.flush();
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    System.out.println(Arrays.asList(sockets));
+                    //if (sockets.get(0).isConnected()) System.out.println(true);
+                    for (Socket element : sockets) {
+                        if (element.isBound()) {
+                            System.out.println(element.getInetAddress().getHostName());
+                        } else {
+                            sockets.remove(element);
+                        }
+                        /*if (!element.isConnected()){
+                            System.out.println("Client " + socket.getInetAddress().getHostName() + " disconnected!");
+                            sockets.remove(element);
+                        }*/
                     }
                 }
-            }, TIME_DELAY, TIME_DELAY);
+            };
+            delay.schedule(sorting, TIME_DELAY, TIME_DELAY);
+            while (true) {
+                socket = serverSocket.accept();
+                sockets.add(socket);
+                System.out.println("Client " + socket.getInetAddress().getHostName() + " connected successfully.");
+            }
+
+            /*delay.schedule(new TimerTask() {
+                @Override
+                public void run() {
+
+                }
+            }, TIME_DELAY, TIME_DELAY);*/
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    /*private void disconnect(DataInputStream in, DataOutputStream out, Socket socket) {
+        try {
+            in.close();
+            out.close();
+            socket.close();
+            System.out.println("Streams and socket are closed!");
+        } catch (IOException e) {
+            System.out.println("Error in closing streams and sockets!");
+        }
+    }*/
 }
